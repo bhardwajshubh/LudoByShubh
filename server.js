09d7c785cf0b3ee52261b5a5909f85f1c311cd30
bhardwajshubh <43513682+bhardwajshubh@users.noreply.github.com>
2020-05-04 18:41:30 +0530

    socket.io config

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const app = require('./app');
+const sockets = require('socket.io')
 const server = http.Server(app);
-
+const io = sockets(server);
+io.on('connection' , socket => {
+    console.log(`socket connected ${socket.id}`);
+})
 
 server.listen(process.env.PORT , () => {
     console.log(`Ludo is running on port ${process.env.PORT}`);
