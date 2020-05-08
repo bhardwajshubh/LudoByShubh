9f745fb1239d42b3dcfe0f40e90378a0bf6197f1
bhardwajshubh <43513682+bhardwajshubh@users.noreply.github.com>
2020-05-08 21:54:29 +0530

    added join room page and added /joinroom path

--- controller/UserController.js
+++ controller/UserController.js
@@ -3,3 +3,7 @@
 exports.createRoom = async (req , res) => {
         res.render('createRoom');
 };
+
+exports.joinRoom = async (req , res) => {
+        res.render('joinRoom');
+};
--- routes/Register/index.js
+++ routes/Register/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('./../../controller/UserController');
 
 router.route('/').get(userController.createRoom);
+router.route('/joinroom').get(userController.joinRoom);
 
 module.exports = router;
