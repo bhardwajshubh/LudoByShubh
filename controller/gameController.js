4c6ef54b2635ecdf62cce42e32a619b0c16f74e9
bhardwajshubh <43513682+bhardwajshubh@users.noreply.github.com>
2020-05-18 00:46:03 +0530

    socket added with room

--- controller/gameController.js
+++ controller/gameController.js
@@ -1,5 +1,14 @@
-
+const Users = require('./../models/Users');
+const Rooms = require('./../models/Rooms');
 
 exports.gamePage = async (req , res) => {
-    res.render('game');
+    const user = req.user;
+    let  rooms;
+    try {
+        rooms = await Rooms.query().select('roomNo').where('id' , user.roomId).first()
+    } catch (e) {
+        console.log(`gameController gamePage ${e}`)
+        return res.send("<h1> 400 : Something went wrong </h1>")
+    }
+    res.render('game' , { room : rooms.roomNo });
 }
--- server.js
+++ server.js
@@ -7,8 +7,13 @@ const server = http.Server(app);
 const io = sockets(server);
 io.on('connection' , socket => {
     console.log(`socket connected ${socket.id}`);
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 })
 
 server.listen(process.env.PORT , () => {
     console.log(`Ludo is running on port ${process.env.PORT}`);
 })
+
+module.exports = io;
